708fcb7d9f7e374db908cb13930dbd1b3fb664f6
Ingrid Jin <ingrid@zaius.com>
2020-05-05 17:41:54 -0400

    updated action count logic, fixed pylint errors

--- zaius/reports/email_metrics.py
+++ zaius/reports/email_metrics.py
@@ -5,13 +5,9 @@ Aggregates email metrics for specified campaign_schedule_run_ts period.
 """
 
 import csv
-import sys
 import datetime
-import json
 
 from .spec import ReportSpec
-from itertools import islice
-
 
 class EmailMetrics(ReportSpec):
     """Email Metrics Report"""
@@ -19,15 +15,13 @@ class EmailMetrics(ReportSpec):
     def register_args(self, parser):
         parser = parser.add_parser(
             "email-metrics",
-            help="individual purchases attributed to the 3-day last touched campaign",
+            help="individual purchases attributed to the 3-day last touched campaign"
         )
-        parser.add_argument("campaign_id", 
-        help = "the ID of the campaign to get metrics for",
-        default = '9097',)
+        parser.add_argument("campaign_id",
+                            help="the ID of the campaign to get metrics for",
+                            default='9097')
         parser.add_argument("start_date", help="earlist date, YYYY-MM-DD, inclusive")
         parser.add_argument("end_date", help="latest date, YYYY-MM-DD, exclusive")
-        
-
         parser.set_defaults(func=self.execute)
 
     # pylint: disable=R0914
@@ -48,7 +42,6 @@ class EmailMetrics(ReportSpec):
         campaign_id = str(args.campaign_id)
         start_date = self._parse_date(args.start_date)
         end_date = self._parse_date(args.end_date)
-        # attribution_days = int(args.attribution_days)
 
         # build our query
         params = {
@@ -92,86 +85,42 @@ class EmailMetrics(ReportSpec):
         # issue the query
         rows = api.query(stmt)
 
-        sends = 0
-        opens = 0
-        clicks = 0
-        unsubs = 0
-        spamreports = 0
+        last_user_id_csrt = None
 
-        saw_open = False
-        saw_send = False
-        saw_clicks = False
-        saw_unsub = False
-        saw_spamreport = False
 
-        last_user_id = None
-        last_csrt = None
-        last_action = None
-        idx = 0
+        seen_actions = {}
+        unique_counts = {}
+        for row in rows:
+            current_user_id_csrt = (row['zaius_id'], row['campaign_schedule_run_ts'])
+            if current_user_id_csrt != last_user_id_csrt:
+            # accumulate
+                for action in seen_actions.keys():
+                    unique_counts[action] = unique_counts.get(action, 0) + 1
+            # reset
+                seen_actions = {}
+        # mark
+            seen_actions[row['action']] = True
+            last_user_id_csrt = current_user_id_csrt
+        # final accumulate to catch whatever user we were processing last
+        for action in seen_actions.keys():
+            unique_counts[action] = unique_counts.get(action, 0) + 1
 
-        for idx, row in enumerate(rows):
-            if idx % 100000 == 0:
-                sys.stderr.write("Read {} rows\n".format(idx))
-            if (
-                (last_user_id != row['zaius_id'] and last_csrt != row['campaign_schedule_run_ts'] and last_action != row['action'])
-                or (last_user_id == row['zaius_id'] and last_csrt != row['campaign_schedule_run_ts'] and last_action != row['action'])
-                or (last_user_id == row['zaius_id'] and last_csrt == row['campaign_schedule_run_ts'] and last_action != row['action'])
-                or (last_user_id != row['zaius_id'] and last_csrt == row['campaign_schedule_run_ts'] and last_action != row['action'])
-                or (last_user_id != row['zaius_id'] and last_csrt == row['campaign_schedule_run_ts'] and last_action == row['action'])
-                or (last_user_id == row['zaius_id'] and last_csrt != row['campaign_schedule_run_ts'] and last_action == row['action'])
-                or (last_user_id != row['zaius_id'] and last_csrt != row['campaign_schedule_run_ts'] and last_action == row['action'])
-                ):
-                last_user_id = row['zaius_id']
-                last_csrt = row['campaign_schedule_run_ts']
-                last_action = row['action']
-                saw_open = False
-                saw_send = False
-                saw_clicks = False
-                saw_unsub = False
-                saw_spamreport = False
-                
-                
-                if row['action'] == 'open':
-                    saw_open = True
-                if row['action'] == 'sent':
-                    saw_send = True
-                if row['action'] == 'click':
-                    saw_clicks = True
-                if row['action'] == 'unsubscribe':
-                    saw_unsub = True
-                if row['action'] == 'spamreport':
-                    saw_spamreport = True
-                if saw_open:
-                    opens += 1
-                if saw_send:
-                    sends += 1
-                if saw_clicks:
-                    clicks += 1
-                if saw_unsub:
-                    unsubs +=1
-                if saw_spamreport:
-                    spamreports += 1
-            elif (last_user_id == row['zaius_id'] and last_csrt == row['campaign_schedule_run_ts'] and last_action == row['action']):
-                opens
-                sends
-                clicks
-                unsubs
-                spamreports
-        
+        # now index into unique counts by action to write the row
         writer.writerow(
-                    {
-                        "total sends": sends,
-                        "unique opens": opens,
-                        "unique clicks": clicks,
-                        "unique unsubscribes": unsubs,
-                        "unique spam reports": spamreports,
-                        "open rate (%)": opens / sends * 100,
-                        "click through rate (%)": clicks / sends * 100,
-                        "unsubscribe rate (%)": unsubs / sends * 100
-                    }
-                )
-        sys.stderr.write("Read {} rows\n".format(idx))
-
+            {
+                "total sends": unique_counts['sent'],
+                "unique opens": unique_counts['open'],
+                "unique clicks": unique_counts['click'],
+                "unique unsubscribes": unique_counts['unsubscribe'],
+                "unique spam reports": unique_counts['spamreport'],
+                "open rate (%)":
+                    unique_counts['open'] / unique_counts['sent'] * 100,
+                "click through rate (%)":
+                    unique_counts['click'] / unique_counts['sent'] * 100,
+                "unsubscribe rate (%)":
+                    unique_counts['unsubscribe'] / unique_counts['sent'] * 100
+            }
+        )
 
     # pylint: disable=R0201
     def _parse_date(self, date_str):
